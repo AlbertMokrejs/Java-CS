185411af7f5a02e7e55ad2be616e27260e3b3b5b
Albert Mokrejs <albertmokrejs@gmail.com>
2014-11-19 16:57:59 -0500

    Wrote final 2 functions, now adds words

--- 03wordsearch/WordSearch.java
+++ 03wordsearch/WordSearch.java
@@ -1,14 +1,43 @@
 public class WordGrid{
     private Char[] data;
+    private int r;
+    private int c;
 
     public WordGrid(int row, int col){
 	data = new Char[row * col];
 	for(int x = 0; x < data.length; x++){
 	    data[x] = " ";}
+	    r = row;
+	    c = col;
     }
 
     public void clear(){
 	for(int x = 0; x < data.length; x++){
 	    data[x] = " ";}}
 
-}
+	public String toString(){
+		String str = "";
+		for(int y = 0; y < r; y++){
+			for(int x = 0; x < data.length()/r; x++){
+				str = "";
+				str += data[x] + " ";
+			}
+		str += "/n";
+		}
+		return str;
+	}
+	
+     public boolean addH(String w, int row, int col){
+     	if((row >= r) || (col + w.length() >= c + 1)){
+     		return false;}
+     	for(int x = 0; x < w.length(); x++){
+     		if((data[c*row + col + x] == w.charAt(x)) || (data[c*row + col + x] == (Char)" ")){
+     			data[c*row + col + x] = w.charAt(x);}
+     		else:
+     			return false;
+     		}
+     		return true;
+     	}
+     	
+     	}
+     }
