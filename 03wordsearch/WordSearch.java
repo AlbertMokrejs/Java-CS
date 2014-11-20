2138ec72bc76021c02c736899cf587c97a7f1e0a
Albert Mokrejs <albertmokrejs@gmail.com>
2014-11-20 13:16:21 -0500

    Updated to add 2 add functions and fix general issue
    general cordinate fix + new functions.

--- 03wordsearch/WordSearch.java
+++ 03wordsearch/WordSearch.java
@@ -1,43 +1,51 @@
 public class WordGrid{
-    private Char[] data;
-    private int r;
-    private int c;
+    private char[][] data;
 
     public WordGrid(int row, int col){
-	data = new Char[row * col];
-	for(int x = 0; x < data.length; x++){
-	    data[x] = " ";}
-	    r = row;
-	    c = col;
+	data = new char[row][col];
+	clear();
     }
 
     public void clear(){
-	for(int x = 0; x < data.length; x++){
-	    data[x] = " ";}}
+	    for(int x = 0; x < data.length; x++){
+	    for(int y = 0; y < data[0].length; y++){
+		data[x][y] = ' ';}}}
 
 	public String toString(){
 		String str = "";
-		for(int y = 0; y < r; y++){
-			for(int x = 0; x < data.length()/r; x++){
-				str = "";
-				str += data[x] + " ";
-			}
-		str += "/n";
-		}
-		return str;
-	}
-	
+		for(int x = 0; x < data.length; x++){
+	    for(int y = 0; y < data[0].length; y++){
+		str += data[x][y] + " ";}}
+		return str;}
+
      public boolean addH(String w, int row, int col){
-     	if((row >= r) || (col + w.length() >= c + 1)){
-     		return false;}
-     	for(int x = 0; x < w.length(); x++){
-     		if((data[c*row + col + x] == w.charAt(x)) || (data[c*row + col + x] == (Char)" ")){
-     			data[c*row + col + x] = w.charAt(x);}
-     		else:
-     			return false;
-     		}
-     		return true;
-     	}
-     	
-     	}
-     }
+	 if((row > data.length) || (col > data[0].length) ||(col + w.length() > data[0].length)){
+	     return false;}
+	 for(int x = 0; x < w.length(); x++){
+	     if((data[row][col+x] == ' ' || (data[row][col+x] == w.charAt(x)))){
+		 data[row][col+x] = w.charAt(x);}
+	     else{
+		 return false;}}
+	 return true;}
+
+    public boolean addV(String w, int row, int col){
+	return addH(w, col, row);}
+
+    public boolean addD(String w, int row, int col){
+	if((row > data.length) || (col > data[0].length) ||(col + w.length() > data[0].length)||(row + w.length() > data.length)){
+	     return false;}
+	 for(int x = 0; x < w.length(); x++){
+	     if((data[row+x][col+x] == ' ' || (data[row+x][col+x] == w.charAt(x)))){
+		 data[row+x][col+x] = w.charAt(x);}
+	     else{
+		 return false;}}
+	 return true;}
+
+    public static void main(String[]args){
+	WordGrid a = new WordGrid(10,10);
+	a.clear();
+	System.out.println(a);
+	a.addH("glub",0,0);
+	a.addV("land",1,0);
+	a.addH("lard",0,1);
+	System.out.println(a);}}
