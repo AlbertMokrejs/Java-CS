7be2362c8ccbfdac34931cfbb18e005aaeb68211
Albert Mokrejs <albertmokrejs@gmail.com>
2014-11-21 09:38:31 -0500

    Multi-directional Adds
    8 directions of adding now possible, with shared code function.

--- 03wordsearch/WordSearch.java
+++ 03wordsearch/WordSearch.java
@@ -18,34 +18,46 @@ public class WordGrid{
 		str += data[x][y] + " ";}}
 		return str;}
 
-     public boolean addH(String w, int row, int col){
-	 if((row > data.length) || (col > data[0].length) ||(col + w.length() > data[0].length)){
-	     return false;}
-	 for(int x = 0; x < w.length(); x++){
-	     if((data[row][col+x] == ' ' || (data[row][col+x] == w.charAt(x)))){
-		 data[row][col+x] = w.charAt(x);}
-	     else{
-		 return false;}}
-	 return true;}
-
-    public boolean addV(String w, int row, int col){
-	return addH(w, col, row);}
+     public boolean addR(String w, int row, int col){
+	 return addZ(w, row, col, 0, 1);}
+	 
+	public boolean addL(String w, int row, int col){
+	return addZ(w, row, col, 0, -1);}
 
     public boolean addD(String w, int row, int col){
-	if((row > data.length) || (col > data[0].length) ||(col + w.length() > data[0].length)||(row + w.length() > data.length)){
+	return addZ(w, row, col, 1,0);}
+	
+public boolean addU(String w, int row, int col){
+	return addZ(w, row, col, -1,0);}
+
+    public boolean addDR(String w, int row, int col){
+	return addZ(w, row, col, 1, 1);}
+	
+	    public boolean addDL(String w, int row, int col){
+	return addZ(w, row, col, 1, -1);}
+	
+		    public boolean addUR(String w, int row, int col){
+	return addZ(w, row, col, -1, 1);}
+	    
+	    public boolean addUL(String w, int row, int col){
+	return addZ(w, row, col, -1, -1);}
+	 
+    public boolean addZ(String w, int row, int col,int a, int b){
+	if((row > data.length) || (col > data[0].length) ||((col + w.length()*b) > data[0].length)||(row + (a*w.length()) > data.length)||(row + (a*w.length()) <0)||(row - (a*w.length()) < 0)){
 	     return false;}
 	 for(int x = 0; x < w.length(); x++){
-	     if((data[row+x][col+x] == ' ' || (data[row+x][col+x] == w.charAt(x)))){
-		 data[row+x][col+x] = w.charAt(x);}
+	     if((data[row+(x*a)][col+(x*b)] == ' ' || (data[row+(x*a)][col+(x*b)] == w.charAt(x)))){
+		 data[row+(x*a)][col+(x*b)] = w.charAt(x);}
 	     else{
 		 return false;}}
 	 return true;}
+
 
     public static void main(String[]args){
 	WordGrid a = new WordGrid(10,10);
 	a.clear();
 	System.out.println(a);
-	a.addH("glub",0,0);
-	a.addV("land",1,0);
-	a.addH("lard",0,1);
+	a.addR("glub",0,0);
+	a.addDR("land",0,1);
+	a.addD("lard",0,1);
 	System.out.println(a);}}
