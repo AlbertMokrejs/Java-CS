7524b2279ad0d7882c216a1c34aefa00caf50552
Albert Mokrejs <albertmokrejs@gmail.com>
2014-11-24 14:21:00 -0500

    Updated to take file inputs
    Uses files to process and add words now. 5 attempts per word. Direction is randomized.

--- 03wordsearch/WordSearch.java
+++ 03wordsearch/WordSearch.java
@@ -1,9 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class WordGrid{
     private char[][] data;
 
     public WordGrid(int row, int col){
 	data = new char[row][col];
 	clear();
+    }
+    
+    public int Rrow(){
+    	return data.length;
+    }
+    public int Rcol(){
+    	return data[0].length;
     }
 
     public void clear(){
@@ -17,30 +28,6 @@ public class WordGrid{
 	    for(int y = 0; y < data[0].length; y++){
 		str += data[x][y] + " ";}}
 		return str;}
-
-     public boolean addR(String w, int row, int col){
-	 return addZ(w, row, col, 0, 1);}
-	 
-	public boolean addL(String w, int row, int col){
-	return addZ(w, row, col, 0, -1);}
-
-    public boolean addD(String w, int row, int col){
-	return addZ(w, row, col, 1,0);}
-	
-public boolean addU(String w, int row, int col){
-	return addZ(w, row, col, -1,0);}
-
-    public boolean addDR(String w, int row, int col){
-	return addZ(w, row, col, 1, 1);}
-	
-	    public boolean addDL(String w, int row, int col){
-	return addZ(w, row, col, 1, -1);}
-	
-		    public boolean addUR(String w, int row, int col){
-	return addZ(w, row, col, -1, 1);}
-	    
-	    public boolean addUL(String w, int row, int col){
-	return addZ(w, row, col, -1, -1);}
 	 
     public boolean addZ(String w, int row, int col,int a, int b){
 	if((row > data.length) || (col > data[0].length) ||((col + w.length()*b) > data[0].length)||(row + (a*w.length()) > data.length)||(row + (a*w.length()) <0)||(row - (a*w.length()) < 0)){
@@ -53,11 +40,23 @@ public boolean addU(String w, int row, int col){
 	 return true;}
 
 
-    public static void main(String[]args){
-	WordGrid a = new WordGrid(10,10);
-	a.clear();
-	System.out.println(a);
-	a.addR("glub",0,0);
-	a.addDR("land",0,1);
-	a.addD("lard",0,1);
-	System.out.println(a);}}
+    public static void main(String args[]) throws FileNotFoundException {
+        File text = new File("C:/temp/test.txt");
+        //	requires a file input string that works
+        Scanner scnr = new Scanner(text);
+        WordGrid a = new Wordgrid(20,20);
+        a.clear();
+        Random r = new Random();
+	ArrayList<String> word = new ArrayList<String>();
+        while(scnr.hasNextLine()){
+            String line = scnr.nextLine();
+            word.add(line);
+        }
+        while(ArrayList.size() > 0){
+        	for(int x = 0; x < 5; x++;){
+        		if(addZ(word.get(0),(int)(r.nextDouble()*Rrow()),(int)(r.nextDouble()*Rcol()),(int)((r.nextDouble()*2)-0.5),(int)((r.nextDouble()*2)-0.5))){
+        			x = 5;
+        		}}
+        		word.remove(0);}
+      	System.out.println(a);
+    }}
