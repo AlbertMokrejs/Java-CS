1a678607f3c7bbc7da4fad301cfd58d9c77c7b3d
Albert Mokrejs <albertmokrejs@gmail.com>
2014-12-10 13:15:11 -0500

    Completed
    Added stuff. It works. Yay.

--- 05GUICtoF/Window2.java
+++ 05GUICtoF/Window2.java
@@ -11,7 +11,7 @@ public class Window2 extends JFrame implements ActionListener{
 
     public Window2() {
  this.setTitle("C to F converter");
- this.setSize(600,400);
+ this.setSize(1200,8000);
  this.setLocation(100,100);
  this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -20,6 +20,8 @@ public class Window2 extends JFrame implements ActionListener{
  pane.setLayout(new FlowLayout());
 
  b = new JButton("All your degree are belong to us.");
+ b.setActionCommand("x");
+ b.addActionListener(this);
  l = new JLabel("Temperature: ",null,JLabel.CENTER);
  t = new JTextField(12);
  c = new JCheckBox("F to C");
@@ -29,7 +31,17 @@ public class Window2 extends JFrame implements ActionListener{
  pane.add(c);
  }
 
-    public void actionPerformed(EventAction e){}
+    public void actionPerformed(ActionEvent e){
+	if(c.isSelected()){
+	    if(t.getText().equals("")){
+		t.setText("0");}
+	    int C = Integer.parseInt(t.getText());
+	    t.setText("" + ((C - 32)*5/9));}
+	    else{
+		if(t.getText().equals("")){
+		    t.setText("0");}
+		    int C = Integer.parseInt(t.getText());
+		    t.setText("" + ((C * 9/5) + 32));}}
 
     public static void main(String[] args) {
         Window2 g = new Window2();
