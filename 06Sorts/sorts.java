76e55791fe2b9d6b85c48a64861cbcb63b31b8a8
Albert Mokrejs <albertmokrejs@gmail.com>
2014-12-16 13:21:41 -0500

    Update sorts.java

--- 06Sorts/sorts.java
+++ 06Sorts/sorts.java
@@ -1,1 +1,27 @@
--
+public class sorts{
+    
+    public static void bubble(int[]c){
+	int P = 0;
+	for(int x = 0; x < c.length - 1; x++){
+	    P = 0;
+	    for(int y = 0; y < c.length - (x+1);y++){
+		if(c[y] > c[y+1]){
+		    int TMP = c[y];
+		    c[y] = c[y+1];
+		    c[y+1] = TMP;
+		    P++;}}
+	    if(P == 0){
+		x = c.length}}}
+
+    public static void insertion(int[]c){
+	for(int x = 1; x < c.length - 1; x++){
+	    for(int y = x; y > 0; y--){
+		if(c[y] < c[y-1]){
+		    int TMP = c[y];
+		    c[y] = c[y+1];
+		    c[y+1] = TMP;}
+		else{
+		    y = 0;}}}}
+
+    public static void selection(int[]c){
+	
