c8bc47b308d9e9ea41550f35adb68141100d3241
Albert Mokrejs <albertmokrejs@gmail.com>
2015-09-29 18:22:43 -0400

    Update app.py

--- temp/app.py
+++ temp/app.py
@@ -3,38 +3,50 @@ from flask import redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/reset")
-def reset():
-    session['n']=0
-    #return redirect("/inc")
-    return redirect(url_for("inc"))
-
-
-@app.route("/inc")
-def inc():
-    if 'n' not in session:
-        session['n']=0
-    n = session['n']
-    n=n+1
-    session['n']=n
-        
-    return render_template("counter.html",n=n)
-
-@app.route("/dec")
-def dec():
-    if 'n' not in session:
-        session['n']=0
-    n = session['n']
-    n=n-1
-    session['n']=n
-        
-    return render_template("counter.html",n=n)
-
 @app.route("/")
-def index():
-    return "hello"
+def home():
+    if "in" not in session:
+        session["in"] = false
+    if "in":
+        mes = """<button><a href = "/secret"> Visit The Secret </a></button><hr>
+            <button><a href = "/logout"> Visit The Secret </a></button>"""
+    else:
+        mes = '<button><a href = "/login"> Login to see the secret </a></button>'
+    return render_template(home.html, m = mes)
+        
 
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if "in" not in session:
+        session["in"] = false
+    if request.method=="GET":
+        return render_template("login.html")
+    else:
+        uname = request.form['user']
+        pword = request.form['pass']
+        button = request.form['button']
+        if uname=="good" and pword=="egg":
+            session["in"] = true
+            return redirect(url_for("secret"))
+        else:
+            error = "Bad egg."
+            return render_template("login.html",err=error)
 
+@app.route("/secret")
+def secret():
+    if "in" not in session:
+        session["in"] = false
+    if not session["in"]:
+        return redirect(url_for(""))
+    else:
+        mes = """The secret to a good omellete is the eggs. <hr>
+            <button><a href = "/logout"> Visit The Secret </a></button>"""
+    return render_template(home.html, m = mes)
+
+@app.route("/logout")
+def out():
+    session["in"] = false
+    return redirect(url_for(""))
 
 if __name__ == "__main__":
    app.debug = True
